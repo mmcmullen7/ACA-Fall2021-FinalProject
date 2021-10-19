548e4aba35e974936cb86f3530b29fb2eeb5a5ab
mmcmullen7 <81434074+mmcmullen7@users.noreply.github.com>
2021-10-19 14:58:46 -0400

    Update finalProject.py

--- finalProject.py
+++ finalProject.py
@@ -33,29 +33,61 @@ def block_audio(x,blockSize,hopSize,fs):
             
     return [xb, timeInSec]
 
- def create_training_dataSet(path, blockSize, hopSize):
+def extract_spectral_flux(xb):
+    
+    [nBlocks_,blockSize_] = xb.shape
+    SpecFluxVector = np.zeros(nBlocks_)
+
+    Hann = np.hanning(blockSize_)
+    Spectrogram = np.abs(np.fft.fft(xb*Hann,2*blockSize_))
+    Spectrogram_ = Spectrogram[:,:blockSize_]
+    Spectrogram_1 = np.concatenate((np.zeros((1,blockSize_)),Spectrogram_),axis=0)
+    
+    SpecDiff = np.diff(Spectrogram_1, axis=0)
+    SpecFluxVector = np.sqrt(np.sum(SpecDiff**2, axis=1))/(blockSize_/2)
+            
+    return SpecFluxVector 
+
+def create_training_dataSet(datasetPath):
      
-     mapping = []
-     for root, dirs, filenames in os.walk(path_to_musicspeech):
+    for root, dirs, filenames in os.walk(datasetPath):
         for directory in dirs:
             if directory == "class":
                 classPath = os.path.join(root, directory)
             elif directory == "beats":
                 beatsPath = os.path.join(root, directory)
-                
-    for root, dirs, filenames in os.walk(classPath);
+
+    classMapping = []                
+    for root, dirs, filenames in os.walk(classPath):
         numFiles = len(filenames)
         training_segment_ndx = np.floor((3/4) * numFiles)
-        classMapping = filenames[0:training_segment_ndx]
+        trainingSegment = filenames[0:training_segment_ndx]
+        for file in trainingSegment:
+            classMapping = classMapping.append(os.path.join(root, file))
     
-    for root, dirs, filenames in os.walk(beatsPath);
+    beatsMapping = []
+    for root, dirs, filenames in os.walk(beatsPath):
         numFiles = len(filenames)
         training_segment_ndx = np.floor((3/4) * numFiles)
-        beatsMapping = filenames[0:training_segment_ndx]
-    
+        trainingSegment = filenames[0:training_segment_ndx]
+        for file in trainingSegment:
+            beatsMapping = beatsMapping.append(os.path.join(root, file))
+        
+    # Determines the average inter beat interval length in seconds for each song
     fileNumber = 1
-    
-    for beatPath in beatsMapping
+    beatLength = np.ones(len(beatsMapping))
+    for i in range(len(beatsMapping)):
+        print("---------Evaluating file", fileNumber, "-----------")
+        beats = np.readtxt(beatsMapping[i])[0]
+        print(beatsMapping[i], ": success read!")
+        ibi = np.mean(beats[1:] - beats[0:-2])
+        beatLength[i] = ibi
+        fileNumber = fileNumber + 1
+        
+    # Reads class annotations for each song and outputs modified class vectors
+    # Each vector corresponds to a class (snare, kick, etc.) and only includes
+    # instances of the given instrument that occur with no other occurences within
+    # +- 1/2 beatLength of that instance    
     for classPath in classMapping:
         
         timeStamp = np.readtxt(txtPath)[:, 0]
